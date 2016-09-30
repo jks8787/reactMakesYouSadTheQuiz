54771d7599d3a0afb1e0343734784ebac036dbf4
Alvin Crespo <alvin.crespo@gmail.com>
2016-09-30 16:03:10 -0400

    Updates the deploy script to unpack build

--- bin/index.js
+++ bin/index.js
@@ -2,22 +2,27 @@
 
 var shell = require('shelljs');
 
-console.log("-----------------Checking out master branch-----------------");
+console.log("-----------------Starting the deploy!-----------------");
+
+console.log("Checking out master branch-----------------");
 shell.exec('git checkout master');
 
-console.log("-----------------Running the build-----------------");
+console.log("Running the build-----------------");
 shell.exec('npm run build');
 
-console.log("-----------------Checking out github pages-----------------");
+console.log("Checking out github pages-----------------");
 shell.exec('git checkout gh-pages');
 
-console.log("-----------------Committing the build-----------------");
+console.log("Moving built files to root-----------------");
+shell.exec('mv build/* .');
+
+console.log("Committing the build-----------------");
 shell.exec("git add -A . && git commit -m 'gh-pages update app'");
 
-console.log("-----------------Committing the build-----------------");
-//shell.exec('git push origin master');
+console.log("Committing the build-----------------");
+shell.exec('git push origin gh-pages');
 
-console.log("-----------------Returning to master branch-----------------");
+console.log("Returning to master branch-----------------");
 shell.exec('git checkout master');
 
 console.log("-----------------Deploy Complete!-----------------");
