089ac9a05da6dd6ea05db77a2d9f2a441eb7fd25
Janice <janice.k.n.smith@gmail.com>
2016-10-03 23:40:26 -0400

    updates to when buttons for question are displayed (#13)

--- src/Question.test.js
+++ src/Question.test.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import appReducer from './reducers';
+import { mount } from 'enzyme';
 
 it('renders a component', () => {
   const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
@@ -28,8 +29,30 @@ it('renders a component', () => {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(appReducer)}>
       <Question
-        question={data}
+        question={data.a}
       />
     </Provider>, div
   );
+});
+
+it('does not render actions if there is no next question', () => {
+  const data = {
+    a: {
+      'value': 'I am a yes or no question.. right?',
+      'steps': {
+        'yes': {
+          'suggestion': null,
+          'nextQuestion': 'a'
+        },
+        'no': {
+          'suggestion': null,
+          'nextQuestion': 'a'
+        }
+      },
+      'id': 'a'
+    }
+  };
+  const wrapper = mount(<Question question={data.a} />);
+
+  expect(wrapper.find('button').length).toEqual(0);
 });
